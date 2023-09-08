ea9538a858aa7a1acb9fbff769e1f5bc4bfc49f7
Charles-Muia <Charles.K.Muia@gmail.com>
2023-09-08 15:21:01 +0300

    Hotel_254

---
+++ index.php
@@ -0,0 +1,153 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hotel_254 | Home</title>
+
+<!-----FAVICON--------------------------------------------------------------------------------------------------------->
+    <link rel="icon" type="image/x-icon" href="Imgs/Logo_1.png">
+
+<!-----CSS------------------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="style.css">
+
+<!-----CDN------------------------------------------------------------------------------------------------------------->
+    <!--    ---BOOTSTRAP-5------>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css">
+
+    <!--    ---BOX-ICONS------>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+    <!--    ---FONT-AWESOME----->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!--    ---OWL-CAROUSEL----->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+<!-----GOOGLE-FONTS---------------------------------------------------------------------------------------------------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+        <div class="container-large bg-white p-0">
+
+<!-----NAV-BAR-SECTION------------------------------------------------------------------------------------------------->
+
+
+<!-----HEADER-SECTION-------------------------------------------------------------------------------------------------->
+
+            <div class="container-fluid p-0 mb-5">
+                <div class="carousel slide" id="header-carousel">
+                    <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                            <img src="Imgs/Background_1.jpg" alt="" class="w-100">
+                                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center bg-transparent">
+                                    <div class="p-3" style="max-width: 700px;">
+                                        <h6 class="section-title text-dark text-uppercase mb-3 animated slideInDown">
+                                            indulge in the exquisite.
+                                        </h6>
+
+                                            <h1 class="display-3 text-dark text-capitalize mb-4 animated slideInDown bg-transparent">
+                                                feel the warmth of hospitality.
+                                            </h1>
+
+                                        <a href="#" class="btn btn-dark text-uppercase py-md-3 px-md-5 animated slideInRight">
+                                            book a room
+                                        </a>
+
+                                    </div>
+                                </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img src="Imgs/Background_2.jpg" alt="" class="w-100">
+                            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center bg-transparent">
+                                <div class="p-3" style="max-width: 700px;">
+                                    <h6 class="section-title text-dark text-uppercase mb-3 animated slideInDown">
+                                        indulge in the exquisite.
+                                    </h6>
+
+                                    <h1 class="display-3 text-dark text-capitalize mb-4 animated slideInDown bg-transparent">
+                                        feel the warmth of hospitality.
+                                    </h1>
+
+                                    <a href="#" class="btn btn-dark text-uppercase py-md-3 px-md-5 animated slideInRight">
+                                        book a room
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img src="Imgs/Background_4.jpg" alt="" class="w-100">
+                            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center bg-transparent">
+                                <div class="p-3" style="max-width: 700px;">
+                                    <h6 class="section-title text-dark text-uppercase mb-3 animated slideInDown">
+                                        indulge in the exquisite.
+                                    </h6>
+
+                                    <h1 class="display-3 text-dark text-capitalize mb-4 animated slideInDown bg-transparent">
+                                        feel the warmth of hospitality.
+                                    </h1>
+
+                                    <a href="#" class="btn btn-dark text-uppercase py-md-3 px-md-5 animated slideInRight">
+                                        book a room
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+
+
+                    <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+
+
+                    <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+
+                </div>
+            </div>
+        </div>
+
+
+
+<!-----JS------------------------------------------------------------------------------------------------------------->
+    <!--    ---BOOTSTRAP-5-JS----->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js"></script>
+
+    <!--    ---JQUERY-JS----->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+
+     <!--    ---OWL-CAROUSEL----->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+
+    <!--    ----JS----->
+    <script>
+
+    </script>
+
+</body>
+</html>
